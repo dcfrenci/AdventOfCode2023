b65d35a400786fecf88da4fb6da0bd09f7de011c
dcfrenci2 <dcfrenci2@gmail.com>
2023-12-07 14:50:27 +0100

    7

--- src/main/java/org/days/Day7.java
+++ src/main/java/org/days/Day7.java
@@ -22,8 +22,8 @@ public class Day7 {
         if (!number.isEmpty()) numbers.add(Integer.parseInt(number.toString()));
         return numbers;
     }
-    private int toCardValue(Character character) {
-        List<Character> value = new ArrayList<>(List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q','K', 'A'));
+    private int toCardValue(Character character, boolean optionTwo) {
+        List<Character> value = optionTwo ? new ArrayList<>(List.of('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q','K', 'A')) : new ArrayList<>(List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q','K', 'A'));
         Map<Character, Integer> strength = new HashMap<>();
         int pos = 0;
         for (Character c : value) {
@@ -31,7 +31,7 @@ public class Day7 {
         }
         return strength.get(character);
     }
-    private int checkCombination(String hand) {
+    private int checkCombination(String hand, boolean optionTwo) {
         List<Character> value = new ArrayList<>(List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q','K', 'A'));
         Map<Character, Integer> strength = new HashMap<>();
         for (Character c : value) {
@@ -39,6 +39,21 @@ public class Day7 {
         }
         for (Character c : hand.toCharArray()) {
             strength.put(c, strength.get(c) + 1);
+        }
+        if (optionTwo) {
+            if (strength.get('J') > 0 && strength.get('J') != 5)  {
+                Integer oldJ = strength.get('J');
+                strength.put('J', 0);
+                Character max = 0;
+                Integer maxValue = 0;
+                for (Map.Entry<Character, Integer> elem : strength.entrySet()) {
+                    if (elem.getValue() > maxValue) {
+                        maxValue = elem.getValue();
+                        max = elem.getKey();
+                    }
+                }
+                strength.put(max, strength.get(max) + oldJ);
+            }
         }
         List<Integer> result = new ArrayList<>();
         for (Integer elem : strength.values()) {
@@ -59,20 +74,20 @@ public class Day7 {
         for (Integer integer : result) {
             if (integer == 2) return 1;
         }
-        return 1;
+        return 0;
     }
-    private int transformToMap(String handOne, String handTwo) {
+    private int transformToMap(String handOne, String handTwo, boolean optionTwo) {
         for (int i = 0; i < handOne.length(); i++) {
-            if (toCardValue(handOne.charAt(i)) > toCardValue(handTwo.charAt(i))) return 1;
-            if (toCardValue(handOne.charAt(i)) < toCardValue(handTwo.charAt(i))) return -1;
+            if (toCardValue(handOne.charAt(i), optionTwo) > toCardValue(handTwo.charAt(i), optionTwo)) return 1;
+            if (toCardValue(handOne.charAt(i), optionTwo) < toCardValue(handTwo.charAt(i), optionTwo)) return -1;
         }
         return 0;
     }
 
-    private long divideLines(List<String> stringList) {
+    private long divideLines(List<String> stringList, boolean optionTwo) {
         Map<String, Integer> hands = new HashMap<>();
         for (String line : stringList) {
-            hands.put(line, checkCombination(line.substring(0, line.indexOf(' '))));
+            hands.put(line, checkCombination(line.substring(0, line.indexOf(' ')), optionTwo));
         }
 
         Map<Integer, List<String>> sorted = new HashMap<>();
@@ -80,7 +95,7 @@ public class Day7 {
             if (sorted.containsKey(elem.getValue())) {
                 List<String> order = new ArrayList<>(sorted.get(elem.getValue()));
                 order.add(elem.getKey());
-                sorted.put(elem.getValue(), order.stream().sorted(this::transformToMap).toList());
+                sorted.put(elem.getValue(), order.stream().sorted((hand1, hand2) -> transformToMap(hand1, hand2, optionTwo)).toList());
             } else {
                 List<String> order = new ArrayList<>();
                 order.add(elem.getKey());
@@ -92,6 +107,7 @@ public class Day7 {
         int strength = 1;
         for (Map.Entry<Integer, List<String>> elem : sorted.entrySet()) {
             for (String string : elem.getValue()) {
+                System.out.println(string + " + " + elem.getKey());
                 ret += (long) strength * numbers(string.substring(string.indexOf(' '))).get(0);
                 strength++;
             }
@@ -106,7 +122,9 @@ public class Day7 {
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         List<String> input = bufferedReader.lines().toList();
         Day7 day7 = new Day7();
-        long ret = day7.divideLines(input);
+        long ret = day7.divideLines(input, false);
         System.out.println("The return value first part: " + ret);
+        ret = day7.divideLines(input, true);
+        System.out.println("The return value second part: " + ret);
     }
 }
